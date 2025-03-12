dd880afba006f03f06c50516adef087f1f3a90fa
Pavel Buylin <buylin399pavel@gmail.com>
2025-03-12 19:35:41 +0200

    The latest version

--- frontend/src/Components/Authorization.jsx
+++ frontend/src/Components/Authorization.jsx
@@ -11,26 +11,30 @@ const Authorization = () => {
 	};
 	
 	return (
-        <div className="authorization-container">
-			<h2>Авторизація</h2>
-			<form onSubmit={handleSubmit}>
-				<label>
-					Логін
-					<input	
-						type="text"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-					/>
-				</label>
-				<label>
-					Пароль
-					<input
-						type="passord"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</label>
-				<button type="submit">Увійти</button>
+        <div className="authorization">
+			<form className="authorization-form" onSubmit={handleSubmit}>
+                <div>
+                    <h2>Авторизація</h2>
+                    <p>Логін</p> 
+                    <div className="inputes">
+                        <input	
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <p>Пароль</p>
+                    <div className="inputes">
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div> 
+                    <div style={{marginTop: "20px", textAlign: "center"}}>
+                        <button type="submit">Увійти</button>
+                    </div>
+                </div>
 			</form>
 		</div>
 	);
