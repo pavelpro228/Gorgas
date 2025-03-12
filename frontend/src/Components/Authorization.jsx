bb0112dfdd0ed8fbd2320e51c7d87b330814e860
Maxim <polshin.maxi@gmail.com>
2025-03-12 19:04:33 +0200

    Форма для авторизации

--- frontend/src/Components/Authorization.jsx
+++ frontend/src/Components/Authorization.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Styles/Authorization.css";
 
 const Authorization = () => {
-    return (
-        <div className="authorization">
-            Authorization
-        </div>
-    )
-}
+    const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log("Логін", username, "Пароль", password);
+	};
+	
+	return (
+        <div className="authorization-container">
+			<h2>Авторизація</h2>
+			<form onSubmit={handleSubmit}>
+				<label>
+					Логін
+					<input	
+						type="text"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+				</label>
+				<label>
+					Пароль
+					<input
+						type="passord"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</label>
+				<button type="submit">Увійти</button>
+			</form>
+		</div>
+	);
+};
 
 export default Authorization;
